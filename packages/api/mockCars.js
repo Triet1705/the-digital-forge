1db0ed914ec325c210b8f10ea63059252543240f
Minh Triet <phoenixmt1705@gmail.com>
2025-08-25 00:59:10 +0700

    feat: demo first for Hero section

--- packages/api/mockCars.js
+++ packages/api/mockCars.js
@@ -3,6 +3,7 @@ export const originalCars = [
   {
     sku: "CAR-911",
     name: "911",
+    descriptionTitle: "The one and always.",
     description: "Iconic sports car with rear engine: 2 doors, 2+2 seats.",
     imageUrl:
       "/assets/images/911/911-carrera/911-carrera/base/911-carrera_gentian-blue_carrera-standard_side.avif",
@@ -10,6 +11,7 @@ export const originalCars = [
   {
     sku: "CAR-TAYCAN",
     name: "Taycan",
+    descriptionTitle: "Overfeel.",
     description: "The soul, electrified: 4 doors, up to 5 seats.",
     imageUrl:
       "/assets/images/taycan/taycan_turbo-s/base/taycan_turbo_s_sport_limousine_sideshot.avif",
@@ -17,6 +19,7 @@ export const originalCars = [
   {
     sku: "CAR-PANAMERA",
     name: "Panamera",
+    descriptionTitle: "More drive. For ambitious destinations.",
     description: "Courage changes everything: 4 doors, 4 seats.",
     imageUrl:
       "/assets/images/panamera/show-case/panamera-4-e-hybrid-model-intro.avif",
@@ -24,6 +27,7 @@ export const originalCars = [
   {
     sku: "CAR-CAYENNE",
     name: "Cayenne",
+    descriptionTitle: "Further together.",
     description: "Versatile, dynamic, and spacious: 5 doors, 5 seats.",
     imageUrl: "/assets/images/cayenne/show-case/cayenne-side.avif",
   },
--- packages/web/src/pages/HomePage.jsx
+++ packages/web/src/pages/HomePage.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import CarSwiper from "../features/homepage/CarSwiper";
+import CarSwiper from "../features/homepage/swiper/CarSwiper";
 import Icon from "../components/ui/icon/icon";
 import Button from "../components/ui/button/Button";
+import HeroSection from "../features/homepage/hero-section/HeroSection";
 
 const HomePage = () => {
   return (
     <div>
+      <HeroSection />
       <CarSwiper />
-
-      <div style={{ padding: "40px", textAlign: "center" }}>
-        <h2>Component Test Area</h2>
-        <Button variant="primary">
-          Test Button <Icon name="arrowRight" size={20} />
-        </Button>
-        <div style={{ marginTop: "20px" }}>
-          <Icon name="arrowLeft" size={30} />
-          <span style={{ margin: "0 20px" }}>Testing Icons</span>
-          <Icon name="arrowRight" size={30} />
-        </div>
-      </div>
     </div>
   );
 };
