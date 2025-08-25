ba76bb9965c0c66ff3814132ba8d3251861eea87
Minh Triet <phoenixmt1705@gmail.com>
2025-08-25 17:23:38 +0700

    feat: Configure a file to map and contains complex icon and apply to icons.js

--- packages/web/src/components/ui/icon/Icon.jsx
+++ packages/web/src/components/ui/icon/Icon.jsx
@@ -2,32 +2,46 @@ import React from "react";
 import clsx from "clsx";
 import { ICONS } from "../../../constants/icons";
 import styles from "./Icon.module.scss";
+import { ICON_MAP } from "./IconMap";
 
 const Icon = ({ name, size = 24, className, ...props }) => {
-  const iconData = ICONS[name];
+  const iconClasses = clsx(styles.icon, className);
 
-  if (!iconData) {
+  const IconComponent = ICON_MAP[name];
+
+  if (IconComponent) {
+    return (
+      <IconComponent
+        width={size}
+        height={size}
+        className={iconClasses}
+        {...props}
+      />
+    );
+  }
+  if (!IconComponent) {
     return null;
   }
 
-  const iconClasses = clsx(styles.icon, className);
+  const simpleIconData = ICONS[name];
+  if (simpleIconData) {
+    return (
+      <svg
+        className={iconClasses}
+        width={size}
+        height={size}
+        viewBox={simpleIconData.viewBox || "0 0 24 24"}
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        {...props}
+      >
+        <path d={simpleIconData.path} />
+      </svg>
+    );
+  }
 
-  return (
-    <svg
-      className={iconClasses}
-      width={size}
-      height={size}
-      viewBox={iconData.viewBox || "0 0 24 24"}
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d={iconData.path} />
-    </svg>
-  );
+  return null;
 };
 
 export default Icon;
---
+++ packages/web/src/components/ui/icon/IconMap.js
@@ -0,0 +1,17 @@
+import FacebookIcon from "../../../assets/icons/FacebookIcon";
+import InstagramIcon from "../../../assets/icons/InstagramIcon";
+import YoutubeIcon from "../../../assets/icons/YoutubeIcon";
+import TwitterIcon from "../../../assets/icons/TwitterIcon";
+import MenuIcon from "../../../assets/icons/MenuIcon";
+import SearchIcon from "../../../assets/icons/SearchIcon";
+import PersonIcon from "../../../assets/icons/PersonIcon";
+
+export const ICON_MAP = {
+  facebook: FacebookIcon,
+  instagram: InstagramIcon,
+  youtube: YoutubeIcon,
+  twitterX: TwitterIcon,
+  menu: MenuIcon,
+  search: SearchIcon,
+  person: PersonIcon,
+};
