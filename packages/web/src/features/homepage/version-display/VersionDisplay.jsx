01554f08e53a56d959fb6b534c2cc3a5097c46c1
Minh Triet <phoenixmt1705@gmail.com>
2025-08-26 18:23:25 +0700

    fix: reduce width of Image, add icon and gap for content in button

--- packages/web/src/features/homepage/version-display/VersionDisplay.jsx
+++ packages/web/src/features/homepage/version-display/VersionDisplay.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./VersionDisplay.module.scss";
 import Button from "../../../components/ui/button/Button";
 import Typography from "../../../components/ui/typography/Typography";
+import Icon from "../../../components/ui/icon/Icon";
 
 const VersionDisplay = ({ versions, selectedVersion, onVersionSelect }) => {
   if (!versions || versions.length === 0) {
@@ -25,7 +26,9 @@ const VersionDisplay = ({ versions, selectedVersion, onVersionSelect }) => {
         {selectedVersion.name}
       </Typography>
       <div className={styles.actions}>
-        <Button variant="primary">Explore The Model</Button>
+        <Button variant="primary">
+          Explore The Model <Icon name="rightArrow"></Icon>
+        </Button>
       </div>
     </div>
   );
