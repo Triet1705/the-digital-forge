beb5349425bf6c8195476483b1c656de16bcda55
Minh Triet <phoenixmt1705@gmail.com>
2025-09-01 16:18:44 +0700

    feat: add variant light, dark to change background color for  header

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./features/layout/header/Header";
 import Footer from "./features/layout/footer/Footer";
 import "./App.css";
 
 function App() {
+  const location = useLocation();
+  const isLightHeader = location.pathname.startsWith("/models");
+
   return (
     <div className="App">
-      <Header />
+      <Header variant={isLightHeader ? "light" : "dark"} />
       <main className="mainContent">
         <Outlet />
       </main>
--- packages/web/src/features/layout/header/Header.jsx
+++ packages/web/src/features/layout/header/Header.jsx
@@ -1,12 +1,13 @@
 // packages/web/src/features/layout/header/Header.jsx
 import React from "react";
-import Icon from "../../../components/ui/icon/icon";
+import Icon from "../../../components/ui/icon/Icon";
 import Typography from "../../../components/ui/typography/Typography";
 import styles from "./Header.module.scss";
+import clsx from "clsx";
 
-const Header = () => {
+const Header = ({ variant = "dark" }) => {
   return (
-    <header className={styles.header}>
+    <header className={clsx(styles.header, styles[variant])}>
       <div className={styles.leftSection}>
         <Icon name="menu" size={28} />
         <Typography variant="body2" className={styles.menuText}>
