521b061f6680f937ffafa82541a0cd9433eff8cc
Minh Triet <phoenixmt1705@gmail.com>
2025-09-01 14:23:43 +0700

    refactor: move into folder to easy maintain

--- packages/web/src/pages/home-page/HomePage.jsx
+++ packages/web/src/pages/home-page/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import CarSwiper from "../features/homepage/swiper/CarSwiper";
-import Icon from "../components/ui/icon/Icon";
-import Button from "../components/ui/button/Button";
-import HeroSection from "../features/homepage/hero-section/HeroSection";
-import ModelGrid from "../features/homepage/model-card/ModelGrid";
+import CarSwiper from "../../features/homepage/swiper/CarSwiper";
+import Icon from "../../components/ui/icon/Icon";
+import Button from "../../components/ui/button/Button";
+import HeroSection from "../../features/homepage/hero-section/HeroSection";
+import ModelGrid from "../../features/homepage/model-card/ModelGrid";
 // import { mockCars } from "../../../api/mockCars";
-import { useCars } from "../hooks/usecases/useCars";
+import { useCars } from "../../hooks/usecases/useCars";
 
 const HomePage = () => {
   const { cars, isLoading, isError } = useCars();
