de53f4adacb6fe041bc3398bf2c1cea5f9fbf5f4
Minh Triet <phoenixmt1705@gmail.com>
2025-09-10 11:59:36 +0700

    feat: update and finish filter sidebar, first level by useState

--- packages/web/src/features/modelspage/FilterSidebar.jsx
+++ packages/web/src/features/modelspage/FilterSidebar.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styles from "./FilterSidebar.module.scss";
 import Typography from "../../components/ui/typography/Typography";
 import Button from "../../components/ui/button/Button";
 import Collapse from "../../components/ui/collapse/Collapse";
 import RadioInput from "../../components/ui/radio/RadioInput";
 import CheckboxInput from "../../components/ui/checkbox/CheckboxInput";
-
-const bodyTypeFilters = [
-  { id: "coupe", name: "Coupé", count: 5 },
-  { id: "roadster", name: "Roadster", count: 5 },
-];
+import {
+  FUEL_TYPE_OPTIONS,
+  BODY_TYPE_OPTIONS,
+  SEAT_OPTIONS,
+} from "../../../constants/filters";
 
 const FilterSidebar = ({
   cars = [],
-  selectedCarSku,
+  filters,
   onFilterChange,
   onResetFilters,
 }) => {
@@ -21,6 +21,35 @@ const FilterSidebar = ({
     (total, car) => total + car.versions.length,
     0
   );
+  const handleCheckboxChange = (event, filterType) => {
+    const { name, checked } = event.target;
+    onFilterChange(filterType, { ...filters[filterType], [name]: checked });
+  };
+
+  const filterCounts = useMemo(() => {
+    if (!cars) return { fuelTypes: {}, bodyTypes: {}, seats: {} };
+
+    const counts = {
+      fuelTypes: Object.fromEntries(FUEL_TYPE_OPTIONS.map((o) => [o.id, 0])),
+      bodyTypes: Object.fromEntries(BODY_TYPE_OPTIONS.map((o) => [o.id, 0])),
+      seats: Object.fromEntries(SEAT_OPTIONS.map((o) => [o.id, 0])),
+    };
+
+    for (const car of cars) {
+      for (const version of car.versions) {
+        car.fuelType?.forEach((type) => {
+          if (counts.fuelTypes[type] !== undefined) counts.fuelTypes[type]++;
+        });
+        version.bodyType?.forEach((type) => {
+          if (counts.bodyTypes[type] !== undefined) counts.bodyTypes[type]++;
+        });
+        version.seats?.forEach((type) => {
+          if (counts.seats[type] !== undefined) counts.seats[type]++;
+        });
+      }
+    }
+    return counts;
+  }, [cars]);
 
   return (
     <div className={styles.sidebar}>
@@ -33,8 +62,8 @@ const FilterSidebar = ({
             label="All"
             name="model"
             value="All"
-            checked={selectedCarSku === "All"}
-            onChange={(e) => onFilterChange(e.target.value)}
+            checked={filters.carSku === "All"}
+            onChange={(e) => onFilterChange("carSku", e.target.value)}
             count={allVersionsCount}
           />
           {cars.map((car) => (
@@ -43,8 +72,8 @@ const FilterSidebar = ({
               label={car.name}
               name="model"
               value={car.sku}
-              checked={selectedCarSku === car.sku}
-              onChange={(e) => onFilterChange(e.target.value)}
+              checked={filters.carSku === car.sku}
+              onChange={(e) => onFilterChange("carSku", e.target.value)}
               count={car.versions.length}
             />
           ))}
@@ -53,12 +82,43 @@ const FilterSidebar = ({
 
       <Collapse title="Body Design">
         <div className={styles.checkboxGroup}>
-          {bodyTypeFilters.map((item) => (
+          {BODY_TYPE_OPTIONS.map((item) => (
             <CheckboxInput
               key={item.id}
               label={item.name}
               name={item.id}
-              count={item.count}
+              count={filterCounts.bodyTypes[item.id]}
+              onChange={(e) => handleCheckboxChange(e, "bodyTypes")}
+            />
+          ))}
+        </div>
+      </Collapse>
+
+      <Collapse title="Seats">
+        <div className={styles.checkboxGroup}>
+          {SEAT_OPTIONS.map((item) => (
+            <CheckboxInput
+              key={item.id}
+              label={item.name}
+              name={item.id}
+              count={filterCounts.seats[item.id]}
+              checked={filters.seats[item.id] || false}
+              onChange={(e) => handleCheckboxChange(e, "seats")}
+            />
+          ))}
+        </div>
+      </Collapse>
+
+      <Collapse title="Fuel Type">
+        <div className={styles.checkboxGroup}>
+          {FUEL_TYPE_OPTIONS.map((item) => (
+            <CheckboxInput
+              key={item.id}
+              label={item.name}
+              name={item.id}
+              count={filterCounts.fuelTypes[item.id]}
+              checked={filters.fuelTypes[item.id] || false}
+              onChange={(e) => handleCheckboxChange(e, "fuelTypes")}
             />
           ))}
         </div>
--- packages/web/src/pages/model-list/ModelListPage.jsx
+++ packages/web/src/pages/model-list/ModelListPage.jsx
@@ -8,17 +8,64 @@ import Layout from "../../components/layout/Layout";
 
 const ModelListPage = () => {
   const { cars, isLoading, isError, error } = useCars();
-  const [selectedCarSku, setSelectedCarSku] = useState("All");
+  const [filters, setFilters] = useState({
+    carSku: "All",
+    fuelTypes: {},
+    bodyTypes: {},
+    seats: {},
+  });
 
   const handleResetFilters = () => {
-    setSelectedCarSku("All");
+    setFilters({
+      carSku: "All",
+      fuelTypes: {},
+      bodyTypes: {},
+      seats: {},
+    });
+  };
+
+  const handleFilterChange = (filterType, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filterType]: value,
+    }));
   };
 
   const filteredCars = useMemo(() => {
     if (!cars) return [];
-    if (selectedCarSku === "All") return cars;
-    return cars.filter((car) => car.sku === selectedCarSku);
-  }, [cars, selectedCarSku]);
+
+    const activeFuelTypes = Object.keys(filters.fuelTypes).filter(
+      (key) => filters.fuelTypes[key]
+    );
+    const activeBodyTypes = Object.keys(filters.bodyTypes).filter(
+      (key) => filters.bodyTypes[key]
+    );
+    const activeSeats = Object.keys(filters.seats).filter(
+      (key) => filters.seats[key]
+    );
+
+    return cars
+      .filter((car) => filters.carSku === "All" || car.sku === filters.carSku)
+      .filter(
+        (car) =>
+          activeFuelTypes.length === 0 ||
+          car.fuelType?.some((type) => activeFuelTypes.includes(type))
+      )
+      .filter(
+        (car) =>
+          activeBodyTypes.length === 0 ||
+          car.versions.some((version) =>
+            version.bodyType?.some((type) => activeBodyTypes.includes(type))
+          )
+      )
+      .filter(
+        (car) =>
+          activeSeats.length === 0 ||
+          car.versions.some((version) =>
+            version.seats?.some((type) => activeSeats.includes(type))
+          )
+      );
+  }, [cars, filters]);
 
   if (isLoading)
     return (
@@ -36,8 +83,8 @@ const ModelListPage = () => {
   const sidebarContent = (
     <FilterSidebar
       cars={cars}
-      selectedCarSku={selectedCarSku}
-      onFilterChange={setSelectedCarSku}
+      filters={filters}
+      onFilterChange={handleFilterChange}
       onResetFilters={handleResetFilters}
     />
   );
